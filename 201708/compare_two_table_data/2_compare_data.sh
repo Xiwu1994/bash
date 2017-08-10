e5db3986de13f45f9272fb5d6a9c9930d2333cf4
xiwu <xiwu@yunniao.me>
2017-08-10 14:33:54 +0800

    fix(update): update

--- 201708/compare_two_table_data/2_compare_data.sh
+++ 201708/compare_two_table_data/2_compare_data.sh
@@ -2,6 +2,7 @@
 while read dir_path
 do
     echo $dir_path >> compare_res
+    echo  >> compare_res
     cat ${dir_path}/data | awk -F"\t" '{
         print "===key===: "$1
         for(i=1;i<=NF/2;i++){
