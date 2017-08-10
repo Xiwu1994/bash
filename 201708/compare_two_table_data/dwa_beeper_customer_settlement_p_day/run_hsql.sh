938fdd0d5e1b9fefa3719b12d2eb22f0edcc097c
xiwu <xiwu@yunniao.me>
2017-08-10 12:03:03 +0800

    fix(update): update

---
+++ 201708/compare_two_table_data/dwa_beeper_customer_settlement_p_day/run_hsql.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+yesterday=`date -d 1day +%F`
+
+####需要修改的配置####
+table_1="dwa_beeper.dwa_beeper_customer_settlement_p_day"
+table_2="decoupling_dwa_beeper.dwa_beeper_customer_settlement_p_day"
+join_key="customer_id"
+is_partition_table=1
+#######################
+
+if [ ${is_partition_table} -eq 0 ]
+then
+  hsql='set hivevar:yesterday="'${yesterday}'";
+  set mapreduce.reduce.memory.mb=4096;
+  set mapreduce.reduce.java.opts=-Xmx4096m;
+  set yarn.app.mapreduce.am.resource.mb=2048;
+  set mapreduce.map.memory.mb=3072;
+  select t1.*, t2.*
+  from '${table_1}' t1
+  left join '${table_2}' t2
+  on t1.'${join_key}' = t2.'${join_key}'
+  where
+  hash(t1.*)
+  <>
+  hash(t2.*) limit 100'
+else
+  hsql='set hivevar:yesterday="'${yesterday}'";
+  set mapreduce.reduce.memory.mb=4096;
+  set mapreduce.reduce.java.opts=-Xmx4096m;
+  set yarn.app.mapreduce.am.resource.mb=2048;
+  set mapreduce.map.memory.mb=3072;
+  select t1.*, t2.*
+  from '${table_1}' t1
+  left join '${table_2}' t2
+  on t1.'${join_key}' = t2.'${join_key}' and t2.p_day=${yesterday}
+  where t1.p_day=${yesterday} and
+  hash(t1.*)
+  <>
+  hash(t2.*) limit 100'
+fi
+
+echo $hsql
+
+#run_hsql
+hive -e "${hsql}" > data
